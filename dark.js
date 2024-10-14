101c2c83302f85238224eb599049caf55eb3a116
Sai Shinkar <141svs@gmail.com>
2024-10-14 18:01:57 +0530

    Added Navigation Bar

---
+++ dark.js
@@ -0,0 +1,23 @@
+
+$(function () {
+  $("#hm-nav").load("header.html", function (response, status, request) {
+    
+    switch (page) {
+      case "home":
+        $(".nav-link").removeClass("active");
+        $(".hm-home").addClass("active bulu");
+        break;
+      case "aichat":
+        $(".nav-link").removeClass("active");
+        $(".hm-aichat").addClass("active bulu");
+
+        break;
+        case "about":
+        $(".nav-link").removeClass("active");
+        $(".hm-about").addClass("active bulu");
+        break;
+    }
+    
+
+  });
+});
